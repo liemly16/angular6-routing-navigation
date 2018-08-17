56b40ea18d13e6ecfd424b3085d0adcd626132f6
Ly Thanh Liem <liembsbs123@gmail.com>
2018-08-17 17:26:50 +0700

    create structure

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes, Router } from '@angular/router';
+import { HeroesComponent } from './pages/heroes/heroes.component'
+import { CrisisCenterComponent } from './pages/crisis-center/crisis-center.component'
+
+const routes: Routes = [
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  },
+  {
+    path: 'crisis-center',
+    component: CrisisCenterComponent
+  },
+  {
+    path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  }
+]
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  declarations: [
+
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CrisisCenterComponent } from './pages/crisis-center/crisis-center.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisCenterComponent,
+    HeroesComponent,
+    NotFoundComponent,
+    AdminComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
