0c149cae4568939825431ce188c159318d9600c3
Ly Thanh Liem <liembsbs123@gmail.com>
2018-08-17 17:35:49 +0700

    add page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes, Router } from '@angular/router';
 import { HeroesComponent } from './pages/heroes/heroes.component'
 import { CrisisCenterComponent } from './pages/crisis-center/crisis-center.component'
+import { NotFoundComponent } from './pages/not-found/not-found.component'
 
 const routes: Routes = [
   {
@@ -17,6 +18,10 @@ const routes: Routes = [
     path: '',
     redirectTo: '/heroes',
     pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
   }
 ]
 
